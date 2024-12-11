e531729652cb9a8988966b8e0f032066126a6cef
Saalimz <ahamedsaalim3639@gmail.com>
2024-12-11 13:20:11 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\ahame\\Desktop\\app developing\\nokia innothon\\flutter_nokia" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Users\\ahame\\Desktop\\app developing\\nokia innothon\\flutter_nokia"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\ahame\\Desktop\\app developing\\nokia innothon\\flutter_nokia\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\ahame\\Desktop\\app developing\\nokia innothon\\flutter_nokia"
+  "FLUTTER_TARGET=C:\\Users\\ahame\\Desktop\\app developing\\nokia innothon\\flutter_nokia\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\ahame\\Desktop\\app developing\\nokia innothon\\flutter_nokia\\.dart_tool\\package_config.json"
+)
